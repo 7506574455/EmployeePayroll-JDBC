aa19f296d50ee6b80bb9db3b61147be816a7374a
7506574455 <sanketlalge09@gmail.com>
2021-08-02 03:34:17 -0700

    Refactor employee payroll update salary using preparedstatement

--- src/main/java/com/jdbc/EmployeePayrollService.java
+++ src/main/java/com/jdbc/EmployeePayrollService.java
@@ -15,21 +15,19 @@ public class EmployeePayrollService {
     public EmployeePayrollService() {
         employeePayrollDBService = EmployeePayrollDBService.getInstance();
     }
+     //To get the list of employee payroll from the database
 
-    // To get the list of employee payroll from the database
-    
     public List<EmployeePayrollData> readEmployeePayrollData(IOService ioService) throws EmployeePayrollException {
         if(ioService.equals(IOService.DB_IO))
             this.employeePayrollList = employeePayrollDBService.readData();
         return this.employeePayrollList;
     }
 
-              // To update the Employee Salary in the database
-         //   If the value is updated, the result value is greater than 0; else 0
-          //   Match the given name with the EmployeePayrollData list
-            //  If found, assign the given salary to the EmployeePayrollData list
-    
-
+   // To update the Employee Salary in the database
+      // If the value is updated, the result value is greater than 0; else 0
+      // Match the given name with the EmployeePayrollData list
+     // If found, assign the given salary to the EmployeePayrollData list
+     
     public void updateEmployeeSalary(String name, double salary) throws EmployeePayrollException {
         int result = employeePayrollDBService.updateEmployeeData(name, salary);
         if(result == 0)
@@ -39,18 +37,17 @@ public class EmployeePayrollService {
             employeePayrollData.salary = salary;
     }
 
-       // To check whether the EmployeePayrollData is in sync with the DB
-      // Use to equals() to compare the values
+          //To check whether the EmployeePayrollData is in sync with the DB
+          //  Use to equals() to compare the values
     
-
     public boolean checkEmployeePayrollInSyncWithDB(String name) throws EmployeePayrollException {
         List<EmployeePayrollData> employeePayrollDataList = employeePayrollDBService.getEmployeePayrollData(name);
         return employeePayrollDataList.get(0).equals(getEmployeePayrollData(name));
     }
 
-         // Check the EmployeePayrollData list for the name
-          // If found, return the value else return null
-     
+ // Check the EmployeePayrollData list for the name
+        //    If found, return the value else return null
+
     private EmployeePayrollData getEmployeePayrollData(String name) {
         return this.employeePayrollList.stream()
                 .filter(employeePayrollDataItem -> employeePayrollDataItem.name.equals(name))
